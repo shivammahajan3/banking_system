2b3a75917738ad33ae319628c058d2c73ee0e72b
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-17 15:19:32 +0530

    optimizing main file for better user response

--- main.rb
+++ main.rb
@@ -1,37 +1,71 @@
-require_relative 'account'
 require_relative 'employee'
-include BankingSystem
+require_relative 'account'
 
 puts "-------------------------------"
 puts "Welcome to the Fraud Bank (^^)"
 puts "-------------------------------"
 
-puts "Select option in Number 1 or 2"
-puts "-------------------------------"
-puts "1. Employee Registration"
-puts "2. New Account"
+loop do
+    puts "Select option in Number 1, 2 or 3"
+    puts "---------------------------------"
+    puts "1. Employee Registration"
+    puts "2. New Account"
+    puts "3. Exit"
 
-option = gets.to_i
+    option = gets.to_i
 
-case option
-when 1
-    puts "Enter Your Name"
-    name = gets
-    puts "Enter Your Email"
-    email = gets
-    puts "Enter Your Password"
-    password = gets
-    Employee.new(name,email,password)
-when 2
-    puts "Enter Your Name"
-    name = gets
-    puts "Enter aadhar Number"
-    aadhar = gets.to_i
-    puts "Enter Contact Number"
-    contact = gets.to_i
-    puts "Deposit Initial Fund"
-    fund = gets.to_i
-    Account.new(name,fund,contact,aadhar)
-else
-    puts "Invalid option. Please choose a valid number (1 or 2)."
+    case option
+    when 1
+        puts "Enter Your Name"
+        name = gets
+        puts "Enter Your Email"
+        email = gets
+        puts "Enter Your Password"
+        password = gets
+        Employee.new(name,email,password)
+    when 2
+        puts "Enter Your Name"
+        name = gets.chomp
+        puts "Enter aadhar Number"
+        aadhar = gets.to_i
+        puts "Deposit Initial Fund"
+        fund = gets.to_i
+        account = Account.new(name,fund,aadhar)
+        loop do
+            puts "--------------------------------------"
+            puts "Select option in Number 1, 2, 3 or 4"
+            puts "--------------------------------------"
+            puts "1. Deposit"
+            puts "2. Withdraw"
+            puts "3. Check balance"
+            puts "4. Exit"
+            
+            account_option = gets.to_i
+
+            case account_option
+            when 1
+              puts "Enter the amount to deposit:"
+              amount = gets.to_f
+              account.deposit(amount)
+            when 2
+              puts "Enter the amount to withdraw:"
+              amount = gets.to_f
+              account.withdraw(amount)
+            when 3
+              account.display_balance
+            when 4
+              puts "Exiting Account Menu..."
+              break
+            else
+              puts "Invalid option. Please choose again."
+            end
+        end
+    when 3
+        puts "Thank you for visiting Fraud Bank. Goodbye!"
+        break
+    else
+        puts "Invalid option. Please choose a valid number (1, 2 or 3)."
+    end
 end
+
+
